6ca5a5eaaef25dd0fb1c655d62ad7d287a26ac6d
Justin Ng <justinng123409@yahoo.com>
2021-05-18 20:49:34 -0700

    changed timer of welcome message

--- fade-in/fade-in.js
+++ fade-in/fade-in.js
@@ -5,13 +5,17 @@ $(function () {
         'My name is <br>Justin Ng'
     
     ];
-    var position = -1;
+    var position = 1;
 
     !function loop() {
+
         position = (position + 1) % header.length;
+
+        var delayTimer = position === 0 ? 1500 : 7500;
+        console.log(delayTimer)
         $header.html(header[position])
         .fadeIn(1000)
-        .delay(7500)
+        .delay(delayTimer)
         .fadeOut(1000, loop);
     }();
 });
